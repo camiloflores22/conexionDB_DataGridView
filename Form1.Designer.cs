023ae4b32b27e9f21f5def6a0c07868f5e27192e
camiloflores22 <camilo.flores.caceres@cftmail.cl>
2023-07-09 18:07:56 -0400

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace conexionDB_DataGridView
+{
+    partial class FormUsuarios
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DGVUsuarios = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVUsuarios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGVUsuarios
+            // 
+            this.DGVUsuarios.AllowUserToAddRows = false;
+            this.DGVUsuarios.AllowUserToDeleteRows = false;
+            this.DGVUsuarios.AllowUserToOrderColumns = true;
+            this.DGVUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVUsuarios.Location = new System.Drawing.Point(12, 12);
+            this.DGVUsuarios.Name = "DGVUsuarios";
+            this.DGVUsuarios.ReadOnly = true;
+            this.DGVUsuarios.RowHeadersWidth = 51;
+            this.DGVUsuarios.RowTemplate.Height = 24;
+            this.DGVUsuarios.Size = new System.Drawing.Size(776, 426);
+            this.DGVUsuarios.TabIndex = 0;
+            // 
+            // FormUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DGVUsuarios);
+            this.Name = "FormUsuarios";
+            this.Text = "USUARIOS";
+            this.Load += new System.EventHandler(this.FormUsuarios_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVUsuarios)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGVUsuarios;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace conexionDB_DataGridView
+{
+    public partial class FormUsuarios : Form
+    {
+        public FormUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void FormUsuarios_Load(object sender, EventArgs e)
+        {
+            string cadenaConexion = "database=prueba;datasource=localhost;port=3308;username=root;password=";
+
+            MySqlConnection conexionDB;
+            DataTable dataTable = new DataTable();
+            MySqlDataReader resultado;
+
+            try
+            {
+                conexionDB = new MySqlConnection(cadenaConexion);
+
+                MySqlCommand command = new MySqlCommand("SELECT * FROM usuario;", conexionDB);
+
+                command.CommandType = CommandType.Text;
+
+                conexionDB.Open();
+
+                resultado = command.ExecuteReader();
+
+                dataTable.Load(resultado);
+            }catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            DGVUsuarios.DataSource = dataTable;
+
+        }
+
+        }
+
+    }
+
+    
+
